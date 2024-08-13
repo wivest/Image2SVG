3100e6f55a8a62c375abdf9301cff203016ca87f
wivest <120594169+wivest@users.noreply.github.com>
2024-08-13 12:20:25 +0200

    feat: check for file in command line parameters

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (args.Length == 0)
+{
+    Console.WriteLine("No file specified.");
+    return 1;
+}
+
+return 0;
