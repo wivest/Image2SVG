cc485ebdd7eac94970ccd0732d5a729e0acac441
wivest <120594169+wivest@users.noreply.github.com>
2024-08-14 23:01:36 +0200

    feat: load image

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
-﻿if (args.Length == 0)
+﻿using SkiaSharp;
+
+if (args.Length == 0)
 {
     Console.WriteLine("No file specified.");
+    return 1;
+}
+
+string filename = args[0];
+SKImage image = SKImage.FromEncodedData(filename);
+if (image == null)
+{
+    Console.WriteLine("Invalid file.");
     return 1;
 }
 
