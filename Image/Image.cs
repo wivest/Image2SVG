2c9116b5323c0ad0ddaae5d4980357f37499e252
wivest <120594169+wivest@users.noreply.github.com>
2024-08-16 23:34:39 +0200

    feat: create Image class

---
+++ Image/Image.cs
@@ -0,0 +1,14 @@
+using SkiaSharp;
+
+namespace Image2SVG.Image
+{
+    class Image
+    {
+        SKImage image;
+
+        public Image(SKImage image)
+        {
+            this.image = image;
+        }
+    }
+}
