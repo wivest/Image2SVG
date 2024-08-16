217975ef50ade6a859e7c2c14667f35138a5d2a2
wivest <120594169+wivest@users.noreply.github.com>
2024-08-16 23:36:46 +0200

    feat: instantiate Image object

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using SkiaSharp;
+﻿using Image2SVG.Image;
+using SkiaSharp;
 
 if (args.Length == 0)
 {
@@ -7,11 +8,13 @@ if (args.Length == 0)
 }
 
 string filename = args[0];
-SKImage image = SKImage.FromEncodedData(filename);
-if (image == null)
+SKImage skImage = SKImage.FromEncodedData(filename);
+if (skImage == null)
 {
     Console.WriteLine("Invalid file.");
     return 1;
 }
+
+var image = new Image(skImage);
 
 return 0;
