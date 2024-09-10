ebf27f21a7f1acf4eabee19ad7878bbf928d9fe2
wivest <120594169+wivest@users.noreply.github.com>
2024-09-10 15:47:31 +0200

    refactor: remove image from fields

--- Application/Image.cs
+++ Application/Image.cs
@@ -6,30 +6,31 @@ namespace Image2SVG.Application
     class Image<T>
         where T : IShape<T>, new()
     {
-        private readonly SKImage image;
         private readonly SKSurface source;
         private readonly SKSurface generated;
         private readonly Generator<T> generator;
 
         public Image(string filename)
         {
-            image = Resize(filename, 0.5f);
-            source = SKSurface.Create(image.Info);
-            source.Canvas.DrawImage(image, 0, 0);
+            SKBitmap bitmap = Resize(filename, 0.5f);
+            source = SKSurface.Create(bitmap.Info);
+            source.Canvas.DrawBitmap(bitmap, 0, 0);
 
-            generated = SKSurface.Create(image.Info);
+            generated = SKSurface.Create(bitmap.Info);
             generated.Canvas.DrawPaint(new SKPaint { Color = SKColors.White });
-            generator = new Generator<T>(image.Info, source, generated);
+            generator = new Generator<T>(bitmap.Info, source, generated);
         }
 
-        public SKImage Resize(string filename, float scale)
+        public SKBitmap Resize(string filename, float scale)
         {
             SKImage source = SKImage.FromEncodedData(filename);
             SKBitmap bitmap = SKBitmap.Decode(filename);
-            var size = new SKImageInfo((int)(source.Width * scale), (int)(source.Height * scale));
+            var size = source.Info.WithSize(
+                (int)(source.Width * scale),
+                (int)(source.Height * scale)
+            );
 
-            bitmap = bitmap.Resize(size, SKFilterQuality.High);
-            return SKImage.FromBitmap(bitmap);
+            return bitmap.Resize(size, SKFilterQuality.High);
         }
 
         public void SaveTo(string filename)
