54afb1ad3d25d441dc2a75b47e1ba8efe308cdbe
wivest <120594169+wivest@users.noreply.github.com>
2024-09-13 01:00:24 +0200

    refactor: return SKBitmap from a method

--- Application/Image.cs
+++ Application/Image.cs
@@ -6,24 +6,22 @@ namespace Image2SVG.Application
     class Image<T>
         where T : IShape<T>, new()
     {
-        private const string RESIZED_FOLDER = "resized/";
-
         private readonly SKSurface source;
         private readonly SKSurface generated;
         private readonly Generator<T> generator;
 
         public Image(string filename)
         {
-            SKImage image = SKImage.FromEncodedData(Resize(filename, 0.5f));
-            source = SKSurface.Create(image.Info);
-            source.Canvas.DrawImage(image, 0, 0);
+            SKBitmap bitmap = Resize(filename, 0.5f);
+            source = SKSurface.Create(bitmap.Info);
+            source.Canvas.DrawBitmap(bitmap, 0, 0);
 
-            generated = SKSurface.Create(image.Info);
+            generated = SKSurface.Create(bitmap.Info);
             generated.Canvas.DrawPaint(new SKPaint { Color = SKColors.White });
-            generator = new Generator<T>(image.Info, source, generated);
+            generator = new Generator<T>(bitmap.Info, source, generated);
         }
 
-        public string Resize(string filename, float scale)
+        public SKBitmap Resize(string filename, float scale)
         {
             SKImage source = SKImage.FromEncodedData(filename);
             SKBitmap bitmap = SKBitmap.Decode(filename);
@@ -31,23 +29,13 @@ namespace Image2SVG.Application
                 (int)(source.Width * scale),
                 (int)(source.Height * scale)
             );
-            bitmap = bitmap.Resize(size, SKFilterQuality.High);
 
-            SKSurface resizedSurface = SKSurface.Create(bitmap.Info);
-            resizedSurface.Canvas.DrawBitmap(bitmap, 0, 0);
-            string resizedFilename = RESIZED_FOLDER + filename;
-            Save(resizedSurface, resizedFilename);
-            return resizedFilename;
+            return bitmap.Resize(size, SKFilterQuality.High);
         }
 
-        public void SaveGenerated(string filename)
+        public void SaveTo(string filename)
         {
-            Save(generated, filename);
-        }
-
-        private void Save(SKSurface surface, string filename)
-        {
-            SKImage generatedImage = surface.Snapshot();
+            SKImage generatedImage = generated.Snapshot();
             using var stream = new FileStream(filename, FileMode.Create);
             generatedImage.Encode().SaveTo(stream);
         }
