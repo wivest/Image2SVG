e77afd9648701e5fe0b70a1b6bf9c5477840db07
wivest <120594169+wivest@users.noreply.github.com>
2024-10-06 23:38:22 +0200

    refactor: separate precalculated average color

---
+++ Application/AverageColor.cs
@@ -0,0 +1,69 @@
+using SkiaSharp;
+
+namespace Image2SVG.Application
+{
+    class AverageColor
+    {
+        private readonly Precalculated redChannel;
+        private readonly Precalculated greenChannel;
+        private readonly Precalculated blueChannel;
+
+        private readonly SKImageInfo info;
+        private readonly SKSurface source;
+
+        public AverageColor(SKImageInfo info, SKSurface source)
+        {
+            this.info = info;
+            this.source = source;
+
+            redChannel = new Precalculated(info);
+            greenChannel = new Precalculated(info);
+            blueChannel = new Precalculated(info);
+            PrecalculateChannel(redChannel, 2);
+            PrecalculateChannel(greenChannel, 1);
+            PrecalculateChannel(blueChannel, 0);
+        }
+
+        public SKColor GetAverageColor(SKRectI rect)
+        {
+            long redSum = redChannel.GetBoundsValue(rect);
+            long greenSum = greenChannel.GetBoundsValue(rect);
+            long blueSum = blueChannel.GetBoundsValue(rect);
+
+            int area = Math.Max(1, rect.Width * rect.Height);
+
+            byte r = (byte)(redSum / area);
+            byte g = (byte)(greenSum / area);
+            byte b = (byte)(blueSum / area);
+
+            return new SKColor(r, g, b);
+        }
+
+        private void PrecalculateChannel(Precalculated channel, int channelIndex)
+        {
+            ReadOnlySpan<byte> sourcePixels = source.PeekPixels().GetPixelSpan();
+
+            long channelRowSum = 0;
+
+            for (int col = 0; col < info.Width; col++)
+            {
+                int pixelIndex = col * info.BytesPerPixel;
+                channelRowSum += sourcePixels[pixelIndex + channelIndex];
+                channel.Data[0, col] = channelRowSum;
+            }
+
+            for (int row = 1; row < info.Height; row++)
+            {
+                int rowIndexOffset = row * info.RowBytes;
+                channelRowSum = 0;
+
+                for (int col = 0; col < info.Width; col++)
+                {
+                    int pixelIndex = rowIndexOffset + col * info.BytesPerPixel;
+                    channelRowSum += sourcePixels[pixelIndex + channelIndex];
+                    channel.Data[row, col] = channel.Data[row - 1, col] + channelRowSum;
+                }
+            }
+        }
+    }
+}
--- Application/Generator.cs
+++ Application/Generator.cs
@@ -11,9 +11,7 @@ namespace Image2SVG.Application
         public readonly SKSurface Generated;
 
         public readonly Precalculated ImageDifference;
-        public readonly Precalculated RedChannel;
-        public readonly Precalculated GreenChannel;
-        public readonly Precalculated BlueChannel;
+        public readonly AverageColor AverageColor;
 
         public Generator(SKImageInfo info, SKSurface source, SKSurface generated)
         {
@@ -22,12 +20,7 @@ namespace Image2SVG.Application
             Generated = generated;
 
             ImageDifference = new Precalculated(info);
-            RedChannel = new Precalculated(info);
-            GreenChannel = new Precalculated(info);
-            BlueChannel = new Precalculated(info);
-            PrecalculateChannel(RedChannel, 2);
-            PrecalculateChannel(GreenChannel, 1);
-            PrecalculateChannel(BlueChannel, 0);
+            AverageColor = new AverageColor(Info, Source);
         }
 
         public T EvolveShapes(int samples, int mutations, int generations, int splits)
@@ -40,7 +33,7 @@ namespace Image2SVG.Application
             {
                 var shape = new T { Info = Info };
                 shape.RandomizeParameters(area);
-                shape.Color = AverageColor(shape.ImageBounds).WithAlpha(128);
+                shape.Color = AverageColor.GetAverageColor(shape.ImageBounds).WithAlpha(128);
                 shapes.Add(shape);
             }
 
@@ -139,48 +132,6 @@ namespace Image2SVG.Application
             }
 
             return worstArea;
-        }
-
-        public void PrecalculateChannel(Precalculated channel, int channelIndex)
-        {
-            ReadOnlySpan<byte> sourcePixels = Source.PeekPixels().GetPixelSpan();
-
-            long channelRowSum = 0;
-
-            for (int col = 0; col < Info.Width; col++)
-            {
-                int pixelIndex = col * Info.BytesPerPixel;
-                channelRowSum += sourcePixels[pixelIndex + channelIndex];
-                channel.Data[0, col] = channelRowSum;
-            }
-
-            for (int row = 1; row < Info.Height; row++)
-            {
-                int rowIndexOffset = row * Info.RowBytes;
-                channelRowSum = 0;
-
-                for (int col = 0; col < Info.Width; col++)
-                {
-                    int pixelIndex = rowIndexOffset + col * Info.BytesPerPixel;
-                    channelRowSum += sourcePixels[pixelIndex + channelIndex];
-                    channel.Data[row, col] = channel.Data[row - 1, col] + channelRowSum;
-                }
-            }
-        }
-
-        public SKColor AverageColor(SKRectI bounds)
-        {
-            long redSum = RedChannel.GetBoundsValue(bounds);
-            long greenSum = GreenChannel.GetBoundsValue(bounds);
-            long blueSum = BlueChannel.GetBoundsValue(bounds);
-
-            int area = Math.Max(1, bounds.Width * bounds.Height);
-
-            byte r = (byte)(redSum / area);
-            byte g = (byte)(greenSum / area);
-            byte b = (byte)(blueSum / area);
-
-            return new SKColor(r, g, b);
         }
     }
 }
