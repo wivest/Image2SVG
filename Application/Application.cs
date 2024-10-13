7fbeca6ef86d350bfa2b82b3efd073d188d14394
wivest <120594169+wivest@users.noreply.github.com>
2024-10-13 23:55:44 +0200

    feat: create load folder

--- Application/Application.cs
+++ Application/Application.cs
@@ -2,6 +2,8 @@ namespace Image2SVG.Application
 {
     class Application
     {
+        private static readonly DirectoryInfo loadFolder = new("images/");
+
         public Application(string[] args) { }
     }
 }
