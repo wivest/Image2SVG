b25da7340194b35b7221948f4421b6688c819921
wivest <120594169+wivest@users.noreply.github.com>
2024-10-27 23:10:29 +0100

    refactor: move calculating to RankItem

--- Application/Rank.cs
+++ Application/Rank.cs
@@ -1,5 +1,4 @@
 using Image2SVG.Shapes;
-using SkiaSharp;
 
 namespace Image2SVG.Application
 {
@@ -22,15 +21,7 @@ namespace Image2SVG.Application
                 shapes,
                 shape =>
                 {
-                    long currentDifference = generator.ImageDifference.GetBoundsValue(
-                        shape.ImageBounds
-                    );
-
-                    using SKSurface currentGeneratedCopy = SKSurface.Create(generator.Info);
-                    currentGeneratedCopy.Canvas.DrawSurface(generator.Generated, 0, 0);
-                    shape.Draw(currentGeneratedCopy.Canvas);
-                    long difference = CalculateDifference(currentGeneratedCopy, shape.ImageBounds);
-                    var item = new RankItem(shape, difference - currentDifference);
+                    var item = new RankItem(shape, generator);
                     Ranked.Add(item);
                 }
             );
@@ -57,33 +48,6 @@ namespace Image2SVG.Application
             }
 
             return shapes;
-        }
-
-        private long CalculateDifference(SKSurface current, SKRectI bounds)
-        {
-            long difference = 0;
-
-            ReadOnlySpan<byte> originalPixels = generator.Source.PeekPixels().GetPixelSpan();
-            ReadOnlySpan<byte> currentPixels = current.PeekPixels().GetPixelSpan();
-
-            int bytesPerRow = generator.Info.RowBytes;
-            int bytesPerPixel = generator.Info.BytesPerPixel;
-
-            for (int y = bounds.Top; y < bounds.Bottom; y++)
-            {
-                int offset = y * bytesPerRow;
-                for (int x = bounds.Left; x < bounds.Right; x++)
-                {
-                    int index = offset + x * bytesPerPixel;
-                    difference += generator.CalculatePixelDifference(
-                        originalPixels,
-                        currentPixels,
-                        index
-                    );
-                }
-            }
-
-            return difference;
         }
     }
 }
--- Application/RankItem.cs
+++ Application/RankItem.cs
@@ -1,25 +1,67 @@
 using Image2SVG.Shapes;
+using SkiaSharp;
 
 namespace Image2SVG.Application
 {
     class RankItem : IComparable<RankItem>
     {
         public IShape Shape;
-        public long Difference;
+        private long difference;
 
-        public RankItem(IShape shape, long difference)
+        private readonly Generator generator;
+
+        public RankItem(IShape shape, Generator generator)
         {
             Shape = shape;
-            Difference = difference;
+            this.generator = generator;
+            Calculate(Shape);
+        }
+
+        private void Calculate(IShape shape)
+        {
+            using SKSurface currentGeneratedCopy = SKSurface.Create(generator.Info);
+            currentGeneratedCopy.Canvas.DrawSurface(generator.Generated, 0, 0);
+            shape.Draw(currentGeneratedCopy.Canvas);
+
+            long currentDifference = generator.ImageDifference.GetBoundsValue(shape.ImageBounds);
+            long newDifference = CalculateDifference(currentGeneratedCopy, shape.ImageBounds);
+            difference = newDifference - currentDifference;
+        }
+
+        private long CalculateDifference(SKSurface current, SKRectI bounds)
+        {
+            long difference = 0;
+
+            ReadOnlySpan<byte> originalPixels = generator.Source.PeekPixels().GetPixelSpan();
+            ReadOnlySpan<byte> currentPixels = current.PeekPixels().GetPixelSpan();
+
+            int bytesPerRow = generator.Info.RowBytes;
+            int bytesPerPixel = generator.Info.BytesPerPixel;
+
+            for (int y = bounds.Top; y < bounds.Bottom; y++)
+            {
+                int offset = y * bytesPerRow;
+                for (int x = bounds.Left; x < bounds.Right; x++)
+                {
+                    int index = offset + x * bytesPerPixel;
+                    difference += generator.CalculatePixelDifference(
+                        originalPixels,
+                        currentPixels,
+                        index
+                    );
+                }
+            }
+
+            return difference;
         }
 
         public int CompareTo(RankItem? other)
         {
             if (other == null)
                 return 0;
-            if (Difference < other.Difference)
+            if (difference < other.difference)
                 return -1;
-            if (Difference > other.Difference)
+            if (difference > other.difference)
                 return 1;
             return 0;
         }
